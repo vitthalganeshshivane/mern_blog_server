a38fec42f1975b9e4d4677bef0c3b6a249092f33
vitthal4545 <vitthalgs1434@gmail.com>
2025-11-07 20:30:10 +0530

    initialize server setup with express, mongoose, firebase-admin and aws s3 config

--- server.js
+++ server.js
@@ -1,128 +1,35 @@
 import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
-import User from "./Schema/User.js";
-import { nanoid } from "nanoid";
-import jwt from "jsonwebtoken";
+import cors from "cors";
+import admin from "firebase-admin";
+import fs from "fs";
+const serviceAccountKey = JSON.parse(
+  fs.readFileSync(
+    "./mernblogwebsite-9b9ff-firebase-adminsdk-fbsvc-538ee207c9.json",
+    "utf8"
+  )
+);
+import { getAuth } from "firebase-admin/auth";
+import aws from "aws-sdk";
 
 const server = express();
-
 let PORT = 3000;
-let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
-let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccountKey),
+});
 
 server.use(express.json());
+server.use(cors());
 
 mongoose
   .connect(process.env.MONGO_URI, { autoIndex: true })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-const formatDatatoSend = (user) => {
-  const access_token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-
-  return {
-    access_token,
-    profile_img: user.personal_info.profile_img,
-    username: user.personal_info.username,
-    fullname: user.personal_info.fullname,
-  };
-};
-
-const generateUsername = async (email) => {
-  let username = email.split("@")[0];
-
-  let isUsernameNotUnique = await User.exists({
-    "personal_info.username": username,
-  }).then((result) => result);
-
-  isUsernameNotUnique ? (username += nanoid().substring(0, 3)) : "";
-
-  return username;
-};
-
-server.post("/signup", (req, res) => {
-  let { fullname, email, password } = req.body;
-
-  //validating the data from the frontend
-  if (fullname.length < 3) {
-    return res
-      .status(403)
-      .json({ error: "Fullname must be at least 3 letters long" });
-  }
-
-  if (!email.length) {
-    return res.status(403).json({ error: "Enter email" });
-  }
-
-  if (!emailRegex.test(email)) {
-    return res.status(403).json({ error: "Email is invalid" });
-  }
-
-  if (!passwordRegex.test(password)) {
-    return res.status(403).json({
-      error:
-        "Password should be 6 to 20 characters long with a numeric, 1 lowercase and 1 uppercase letters",
-    });
-  }
-
-  bcrypt.hash(password, 10, async (err, hashed_Password) => {
-    let username = await generateUsername(email);
-
-    let user = new User({
-      personal_info: {
-        fullname,
-        email,
-        password: hashed_Password,
-        username,
-      },
-    });
-
-    user
-      .save()
-      .then((u) => {
-        return res.status(200).json(formatDatatoSend(u));
-      })
-      .catch((err) => {
-        if (err.code === 11000) {
-          return res.status(500).json({ error: "email already exists !" });
-        }
-
-        return res.status(500).json({ error: err.message });
-      });
-  });
-});
-
-server.post("/signin", (req, res) => {
-  let { email, password } = req.body;
-
-  User.findOne({ "personal_info.email": email })
-    .then((user) => {
-      if (!user) {
-        return res.status(403).json({ error: "Email not found" });
-      }
-
-      bcrypt.compare(password, user.personal_info.password, (err, result) => {
-        if (err) {
-          return res
-            .status(403)
-            .json({ error: "Error occured while login please try again" });
-        }
-
-        if (!result) {
-          return res.status(403).json({ error: "Incorrect Password" });
-        } else {
-          return res.status(200).json(formatDatatoSend(user));
-        }
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      return res.status(403).json({ error: err.message });
-    });
-});
-
-server.listen(PORT, () => {
-  console.log(`listening on port -> ${PORT}`);
+const s3 = new aws.S3({
+  region: "ap-south-1",
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
