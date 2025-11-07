5ed3c07e1bdc0b3ab51e5fafd3dbddbd7f451b61
vitthal4545 <vitthalgs1434@gmail.com>
2025-11-07 20:41:07 +0530

    add signup API with input validation, password hashing, and unique username generation

--- server.js
+++ server.js
@@ -81,6 +81,58 @@ const generateUsername = async (email) => {
   return username;
 };
 
+server.post("/signup", (req, res) => {
+  let { fullname, email, password } = req.body;
+
+  //validating the data from the frontend
+  if (fullname.length < 3) {
+    return res
+      .status(403)
+      .json({ error: "Fullname must be at least 3 letters long" });
+  }
+
+  if (!email.length) {
+    return res.status(403).json({ error: "Enter email" });
+  }
+
+  if (!emailRegex.test(email)) {
+    return res.status(403).json({ error: "Email is invalid" });
+  }
+
+  if (!passwordRegex.test(password)) {
+    return res.status(403).json({
+      error:
+        "Password should be 6 to 20 characters long with a numeric, 1 lowercase and 1 uppercase letters",
+    });
+  }
+
+  bcrypt.hash(password, 10, async (err, hashed_Password) => {
+    let username = await generateUsername(email);
+
+    let user = new User({
+      personal_info: {
+        fullname,
+        email,
+        password: hashed_Password,
+        username,
+      },
+    });
+
+    user
+      .save()
+      .then((u) => {
+        return res.status(200).json(formatDatatoSend(u));
+      })
+      .catch((err) => {
+        if (err.code === 11000) {
+          return res.status(500).json({ error: "email already exists !" });
+        }
+
+        return res.status(500).json({ error: err.message });
+      });
+  });
+});
+
 server.listen(PORT, () => {
   console.log(`listening on port -> ${PORT}`);
 });
