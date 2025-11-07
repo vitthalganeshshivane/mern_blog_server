28549901e02871482f2473b93e41955a5c2a8332
vitthal4545 <vitthalgs1434@gmail.com>
2025-11-07 20:33:23 +0530

    add JWT authentication middleware, email/password validation, and User model import

--- server.js
+++ server.js
@@ -13,6 +13,31 @@ const serviceAccountKey = JSON.parse(
 import { getAuth } from "firebase-admin/auth";
 import aws from "aws-sdk";
 
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { nanoid } from "nanoid";
+import User from "./Schema/User.js";
+
+let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ error: "No access token" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: "Access token is invalid" });
+    }
+    req.user = user.id;
+    next();
+  });
+};
+
 const server = express();
 let PORT = 3000;
 
