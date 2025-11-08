980f8255732a4920d6a14864cc53d73a1cad52ec
vitthal4545 <vitthalgs1434@gmail.com>
2025-11-08 21:46:49 +0530

    add create and edit blog functionality with draft support

--- server.js
+++ server.js
@@ -233,6 +233,101 @@ server.post("/google-auth", async (req, res) => {
   }
 });
 
+server.post("/create-blog", verifyJWT, (req, res) => {
+  let authorId = req.user;
+
+  let { title, des, banner, tags, content, draft, id } = req.body;
+
+  if (!title.length) {
+    return res.status(403).json({ error: "You must provide a title" });
+  }
+
+  if (!draft) {
+    if (!des.length || des.length > 200) {
+      return res.status(403).json({
+        error: "You must provide blog description under 200 characters",
+      });
+    }
+
+    if (!banner.length) {
+      return res
+        .status(403)
+        .json({ error: "You must provide a banner to publish it." });
+    }
+
+    if (!content.blocks.length) {
+      return res
+        .status(403)
+        .json({ error: "There must be some blog content to publish it" });
+    }
+
+    if (!tags.length || tags.length > 200) {
+      return res.status(403).json({
+        error: "Provide tags in order to publish the blog, Maximum 10",
+      });
+    }
+  }
+
+  tags = tags.map((tag) => {
+    return tag.toLowerCase();
+  });
+
+  let blog_id =
+    id ||
+    title
+      .replace(/[^a-zA-Z0-9]/g, " ")
+      .replace(/\s+/g, "-")
+      .trim() + nanoid();
+
+  if (id) {
+    Blog.findOneAndUpdate(
+      { blog_id },
+      { title, des, banner, content, tags, draft: draft ? draft : false }
+    )
+      .then(() => {
+        return res.status(200).json({ id: blog_id });
+      })
+      .catch((err) => {
+        return res.status(500).json({ error: err.message });
+      });
+  } else {
+    let blog = new Blog({
+      title,
+      des,
+      banner,
+      content,
+      tags,
+      author: authorId,
+      blog_id,
+      draft: Boolean(draft),
+    });
+
+    blog
+      .save()
+      .then((blog) => {
+        let incrementVal = draft ? 0 : 1;
+        User.findOneAndUpdate(
+          { _id: authorId },
+          {
+            $inc: { "account_info.total_posts": incrementVal },
+            $push: { blogs: blog._id },
+          }
+        )
+          .then((user) => {
+            return res.status(200).json({ id: blog.blog_id });
+          })
+          .catch((err) => {
+            return res
+              .status(500)
+              .json({ error: "failed to update total post numbers" });
+          });
+      })
+      .catch((err) => {
+        return res.status(500).json({ error: err.message });
+      });
+  }
+});
+
 server.listen(PORT, () => {
   console.log(`listening on port -> ${PORT}`);
 });
