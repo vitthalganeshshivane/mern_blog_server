474d4ec1cf9a7bbbb49a86dfa4263597e7f746e0
vitthal4545 <vitthalgs1434@gmail.com>
2025-11-08 21:47:57 +0530

    add get blog endpoint with read count tracking

--- server.js
+++ server.js
@@ -328,6 +328,41 @@ server.post("/create-blog", verifyJWT, (req, res) => {
   }
 });
 
+server.post("/get-blog", (req, res) => {
+  let { blog_id, draft, mode } = req.body;
+
+  let incrementVal = mode !== "edit" ? 1 : 0;
+
+  Blog.findOneAndUpdate(
+    { blog_id },
+    { $inc: { "activity.total_reads": incrementVal } }
+  )
+    .populate(
+      "author",
+      "personal_info.fullname personal_info.username personal_info.profile_img"
+    )
+    .select("title des content banner activity publishedAt blog_id tags")
+    .then((blog) => {
+      User.findOneAndUpdate(
+        {
+          "personal_info.username": blog.author.personal_info.username,
+        },
+        { $inc: { "account_info.total_reads": incrementVal } }
+      ).catch((err) => {
+        return res.status(500).json({ error: err.message });
+      });
+
+      if (blog.draft && !draft) {
+        return res.status(500).json({ error: "You cannot access draft blog" });
+      }
+
+      return res.status(200).json({ blog });
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: err.message });
+    });
+});
+
 server.listen(PORT, () => {
   console.log(`listening on port -> ${PORT}`);
 });
