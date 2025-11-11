1d3b275e166d9e1dd72e27fc2e1056e580c3db8b
vitthal4545 <vitthalgs1434@gmail.com>
2025-11-11 20:00:34 +0530

    add API endpoint to generate and return S3 upload URL

--- server.js
+++ server.js
@@ -93,6 +93,16 @@ const generateUsername = async (email) => {
   return username;
 };
 
+//upload image url route
+server.get("/get-upload-url", (req, res) => {
+  generateUploadURL()
+    .then((url) => res.status(200).json({ uploadURL: url }))
+    .catch((err) => {
+      console.log(err.message);
+      return res.status(500).json({ error: err.message });
+    });
+});
+
 server.post("/signup", (req, res) => {
   let { fullname, email, password } = req.body;
 
