919454dbe4e8032fb5513fce5af46865238565e5
vitthal4545 <vitthalgs1434@gmail.com>
2025-11-12 23:15:31 +0530

    add API to post blog comments with JWT auth and notification

--- server.js
+++ server.js
@@ -563,6 +563,56 @@ server.post("/get-profile", (req, res) => {
     });
 });
 
+server.post("/add-comment", verifyJWT, (req, res) => {
+  let user_id = req.user;
+
+  let { _id, comment, replying_to, blog_author } = req.body;
+
+  if (!comment.length) {
+    return res
+      .status(403)
+      .json({ error: "Write something to leave a comment" });
+  }
+
+  let commentObj = new Comment({
+    blog_id: _id,
+    blog_author,
+    comment,
+    commented_by: user_id,
+  });
+
+  commentObj.save().then((commentFile) => {
+    let { comment, commentedAt, children } = commentFile;
+
+    Blog.findOneAndUpdate(
+      { _id },
+      {
+        $push: { comments: commentFile._id },
+        $inc: { "activity.total_comments": 1 },
+        "activity.total_parent_comments": 1,
+      }
+    ).then((blog) => {
+      console.log("New comment added");
+    });
+
+    let notificationObj = {
+      type: "comment",
+      blog: _id,
+      notification_for: blog_author,
+      user: user_id,
+      comment: commentFile._id,
+    };
+
+    new Notification(notificationObj).save().then((notification) => {
+      console.log("new notification created");
+    });
+
+    return res
+      .status(200)
+      .json({ comment, commentedAt, _id: commentFile._id, user_id, children });
+  });
+});
+
 server.listen(PORT, () => {
   console.log(`listening on port -> ${PORT}`);
 });
